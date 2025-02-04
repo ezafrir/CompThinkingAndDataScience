2bbbf458227ae14dd246484b62e67ee73d9c4df2
zafrir <124220741+ezafrir@users.noreply.github.com>
2025-02-04 13:37:01 -0500

    Create 05.2_genEven.py
    Unit 2; Lecture 5 - Exercise 2

---
+++ Unit 2/05.2_genEven.py
@@ -0,0 +1,16 @@
+"""
+This problem asks you to write a short function that uses the the random module. 
+How would you randomly generate an even number x, 0 <= x < 100? Fill out the definition for the function genEven(). 
+Please generate a uniform distribution over the even numbers between 0 and 100 (not including 100).
+"""
+import random
+def genEven():
+    '''
+    Returns a random even number x, where 0 <= x < 100
+    '''
+import random
+def genEven():
+    '''
+    Returns a random even number x, where 0 <= x < 100
+    '''
+    return random.randrange(0, 100, 2)
