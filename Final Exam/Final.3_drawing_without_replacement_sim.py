08225fb949fbb27828b42920022cee38fcff41d2
zafrir <124220741+ezafrir@users.noreply.github.com>
2025-02-10 11:03:46 -0500

    Create Final.3_drawing_without_replacement_sim.py

---
+++ Final Exam/Final.3_drawing_without_replacement_sim.py
@@ -0,0 +1,16 @@
+import random
+
+
+def drawing_without_replacement_sim(numTrials):
+    counter = 0
+    for i in range(numTrials):
+        bucket = ['R', 'R', 'R', 'R', 'G', 'G', 'G', 'G']
+        picks = []
+        for j in range(3):
+            k = random.choice(bucket)
+            picks.append(k)
+            bucket.remove(k)
+        if picks[0] == picks[1] == picks[2]:
+            counter += 1
+    return counter/numTrials                    
+    
